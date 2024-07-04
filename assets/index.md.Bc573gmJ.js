ed3de12a93cdd5f627e3f150eceef6efb3e03cc3
nobody <nobody@human.com>
2024-07-04 23:42:32 +0800

    feat: builder

---
+++ assets/index.md.Bc573gmJ.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.BzzDRghR.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"page"},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),n={name:"index.md"};function o(r,s,c,d,i,p){return a(),t("div")}const f=e(n,[["render",o]]);export{m as __pageData,f as default};
